aa9b3b8e6b5c05f28d5a3ff3a66bfdc9a945986f
Vladimir Nitochkin <vovnit@gmail.com>
2020-08-20 23:04:36 +0300

    init project

---
+++ StoryBard/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  StoryBard
+//
+//  Created by Vladimir Nitochkin on 20.08.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
